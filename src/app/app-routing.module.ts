73dc0198bf3a9ca19b3f49489a4e5c64c620db57
xuanhuy <huynguyen@asimtelecom.vn>
2021-06-26 09:29:09 +0700

    sss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HeroComponent} from './hero/hero.component'
+import {AppComponent} from './app.component'
 
 const routes: Routes = [
+    {path: '', component: AppComponent},
     {path: 'heroes', component: HeroComponent},
 ];
 
